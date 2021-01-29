955a731ef26fe29b7847ac0d54f7b0312e2e5f83
Horrendous15 <48510960+Horrendous15@users.noreply.github.com>
2021-01-29 11:35:11 +0300

    Update test_sidebar_receipts.py

--- tests/test_sidebar_receipts.py
+++ tests/test_sidebar_receipts.py
@@ -156,14 +156,14 @@ class TestMenuReceipts():
         try:
             WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR,
                                                                                   ".col-12.widget-receipts-history")))
-            remove_folder(f"{config['path_to_download']}")
+#             remove_folder(f"{config['path_to_download']}")
 
             download_click = driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .getReceipt")
 
             download_click.click()
             time.sleep(2)
 
-            assert file_in_dir(f"{config['path_to_download']}/*.pdf")
+            assert file_in_dir(f"{config['path_to_download']}")
         except TimeoutException:
             assert check_exists_by_xpath(".col-12.widget-receipts-history", driver)
 
@@ -173,13 +173,13 @@ class TestMenuReceipts():
         try:
             WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR,
                                                                                    ".col-12.widget-receipts-history")))
-            remove_folder(f"{config['path_to_download']}")
+#             remove_folder(f"{config['path_to_download']}")
 
             download_click = driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .getReceipt")
             download_click.click()
             time.sleep(3)
 
-            assert read_file(f"{config['path_to_download']}/*.pdf")
+            assert read_file(f"{config['path_to_download']}")
 
         except TimeoutException:
             assert check_exists_by_xpath(".col-12.widget-receipts-history", driver)

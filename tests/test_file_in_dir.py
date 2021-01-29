786feb02ccccc7c9ee510f8bf21a1a3139281fde
Horrendous15 <48510960+Horrendous15@users.noreply.github.com>
2021-01-29 14:40:39 +0300

    Update test_file_in_dir.py

--- tests/test_file_in_dir.py
+++ tests/test_file_in_dir.py
@@ -15,41 +15,14 @@ from allure_commons.types import AttachmentType
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-# проверка существования элемента
-def check_exists_by_xpath(xpath, driver):
-    try:
-        block_sch = driver.find_element(By.CSS_SELECTOR, xpath)
-    except NoSuchElementException:
-        return False
-    return True
-
-
 # # чтение файла
 # def read_file(path):
-#     try:
-#         for f in os.listdir(path):
-#             if f.endswith(".pdf"):
-#                 fl = open(f)
-#                 fl.close()
-#     except FileNotFoundError:
-#         return False
-#     return True
-
-
-# # удаление файлов
-# def remove_folder(path):
-#     for f in os.listdir(path):
-#         if f.endswith(".pdf"):
-#             os.remove(f)
-
-# чтение файла
-def read_file(path):
-    os.chdir(path)
-    for file in glob.glob("*.pdf"):
-        fl = open(file)
-        fl.close()
-        return True
-    return False
+#     os.chdir(path)
+#     for file in glob.glob("*.pdf"):
+#         fl = open(file)
+#         fl.close()
+#         return True
+#     return False
 
 def file_in_dir(path):
     os.chdir(path)
@@ -64,6 +37,6 @@ class TestReceipts():
     def test_download_file(self, config):
         assert file_in_dir(f"{config['path_to_download']}")
 
-    # является ли файл доступным и читаемым
-    def test_read_file(self, config):
-        assert read_file(f"{config['path_to_download']}")
+#     # является ли файл доступным и читаемым
+#     def test_read_file(self, config):
+#         assert read_file(f"{config['path_to_download']}")
